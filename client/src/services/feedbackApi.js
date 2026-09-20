b7690ca76976c903881080d27ff0bbd1b2dc690a
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-09-21 01:50:32 +0530

    fix:root problem

--- client/src/services/feedbackApi.js
+++ client/src/services/feedbackApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const feedbackApi = async (input, rating) => {
-  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/feedback`, {
+  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/feedback`, {
     name: input.name,
     comment: input.comment,
     rating,
@@ -11,6 +11,6 @@ export const feedbackApi = async (input, rating) => {
 };
 
 export const getAllFeedback = async () => {
-  const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/feedback`);
+  const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/feedback`);
   return res.data;
 };
